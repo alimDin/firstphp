f0155e2ccd538bbd9ae0f728230637666731cb05
Alim Din <ad432@njit.edu>
2017-10-10 22:03:26 -0400

    change

--- index.php
+++ index.php
@@ -11,13 +11,38 @@
     $date1 =  date('Y/m/d', time());
     echo "The value of \$date: ".$date."<br>";
 
-    if(strtotime($date) < strtotime($dtar))
+    if
+           (strtotime($date) < strtotime($dtar))
     	 {
             echo "The Past!";
+         }
+    elseif (strtotime($date) == strtotime($dtar)) 
+         {
+            echo "Same Day";
          }
     else
          {
             echo "The Future!";
          }
+
+   
+    $character = "/";
+    $lastPos = 0;
+    $positions = array();
+
+     while (($lastPos = strpos($date, $character, $lastPos))!== false) 
+     {
+    $positions[] = $lastPos;
+    $lastPos = $lastPos + strlen($character);
+     }
+
+     foreach ($positions as $value) {
+         echo $value ."<br />";
+    }
+
+// Displays 3 and 10
+foreach ($positions as $value) {
+    echo $value ."<br />";
+}
 
 ?>
