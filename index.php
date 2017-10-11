2a938ee42cdd75e62f997f97b5c4c881245909dc
Alim Din <ad432@njit.edu>
2017-10-10 21:20:06 -0400

    hello

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
 	$year = array("2012", "396", "300","2000", "1100", "1089");
 	echo "The value of \$year: ";
 	print_r($year);
-	echo "<br>";
-	echo "<br>";
+	
 
     $date =  date('Y/m/d', time());
     echo "The value of \$date: ".$date."<br>";
